ed391746a2e97fa5da9b5cdf97cd4750f2c90764
paarkesh <87378028+Paarkesh@users.noreply.github.com>
2022-12-12 09:57:41 +0530

    first commit

---
+++ login_application/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'welcome_page.dart';
+import 'home_page.dart';
+import 'signup_page.dart';
+import 'login_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: 'welcome_screen',
+      routes: {
+        'welcome_screen': (context) => WelcomeScreen(),
+        'registration_screen': (context) => RegistrationScreen(),
+        'login_screen': (context) => LoginScreen(),
+        'home_screen': (context) => HomeScreen()
+      },
+    );
+  }
+}
